287fcc793c3d47c5400b1b391bd2a3b21fa6c040
Xavi Fortes <itsfortes@gmail.com>
2023-05-26 06:35:08 +0200

    Add project files.

---
+++ RobloxEdit/Globals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobloxUnlocker
+{
+    public static class Globals
+    {
+        public static string VersionsPath { get; set; }
+        public static string AppVersionPath { get; set; }
+        public static string AppConfigPath { get; set; }
+        public static readonly string AppDataFolder = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "XaviFortes", "RobloxUnlocker");
+    }
+}
